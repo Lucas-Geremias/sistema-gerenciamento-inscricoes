57ae799f2226e3dbd3cb46febc3cf688138db833
lucas-geremias <geremiaslucas2012@gmail.com>
2025-02-20 21:46:30 -0300

    aula03

--- src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -2,6 +2,7 @@ package br.com.nlw.events.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import br.com.nlw.events.exception.SubscriptionConflictException;
 import br.com.nlw.events.exception.UserIndicadorNotfoundException;
 import br.com.nlw.events.model.User;
 import br.com.nlw.events.service.SubscriptionService;
+
 
 @RestController
 public class SubscriptionController {
@@ -42,4 +44,22 @@ public class SubscriptionController {
     return ResponseEntity.badRequest().build();
 
   }
+  @GetMapping("/subscription/{prettyName}/ranking")
+  public ResponseEntity<?> generateRankingByEvent(@PathVariable String prettyName) {
+    try{
+      return ResponseEntity.ok(service.getCompleteRanking(prettyName).subList(0, 3));
+
+    }catch(EventNotFoundException e){
+      return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
+
+    }
+  }
+  @GetMapping("/subscription/{prettyName}/ranking/{userId}")
+    public ResponseEntity<?> generateRaningByEventAndUser(@PathVariable String prettyName, @PathVariable Integer userId){
+        try{
+            return ResponseEntity.ok(service.getRankingByUser(prettyName, userId));
+        }catch(Exception ex){
+            return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
+        }
+    } 
 }
